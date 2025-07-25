beafd96256ac15e399f2d460bfe80df7ad6ef0d9
AbuBakr-03 <abubakr399977@gmail.com>
2025-07-25 16:14:44 +0500

    Added interview table.

---
+++ smarthr-frontend/src/apps/predicted-tables/PredictedTable.tsx
@@ -0,0 +1,15 @@
+import { useListPredictionsPrivate } from "@/hooks/usePredicted";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
+const DepartmentTable = () => {
+  const listPredicted = useListPredictionsPrivate();
+  return (
+    <div className="container mx-auto rounded-xl border-1 px-5 py-5">
+      <h1 className="pb-4 text-2xl font-semibold">Predicted Candidates</h1>
+      <DataTable columns={columns} data={listPredicted.data ?? []} />
+    </div>
+  );
+};
+
+export default DepartmentTable;
